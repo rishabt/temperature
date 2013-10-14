73921d70c2a30f2a7803068dc89bb0082bceded7
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-10-14 19:13:06 -0400

    Changed some variable names

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -13,11 +13,11 @@ public class TemperatureTest {
   // {@Code Temperature} class.
 
    @Test
-   public void test_celsiusToKelvin(){
+   public void test_celsiusToKelvin(){											//Tests conversion from Celcius to Kelvin
 	   System.out.println("Test conversion from Celsius to Kelvin....") ;
 	   
-	   Temperature averageHighInOctober = new Temperature (0.0, Temperature.Units.CELSIUS);
-	   Temperature converted = new Temperature(averageHighInOctober);
+	   Temperature temp = new Temperature (0.0, Temperature.Units.CELSIUS);		//Easiest case of converting 0 Celcius to Kelvin
+	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.KELVIN);
 	   
 	   assertTrue(converted.getValue() == 273.15) ;
@@ -27,8 +27,8 @@ public class TemperatureTest {
    public void test_kelvinToCelsius(){
 	   System.out.println("Test conversion from Kelvin to Celsius....") ;
 	   
-	   Temperature averageHighInOctober = new Temperature (273.15, Temperature.Units.KELVIN);
-	   Temperature converted = new Temperature(averageHighInOctober);
+	   Temperature temp = new Temperature (273.15, Temperature.Units.KELVIN);
+	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.CELSIUS);
 	   
 	   assertTrue(converted.getValue() == 0.0) ;
@@ -39,8 +39,8 @@ public class TemperatureTest {
    public void test_fahrenheitToKelvin(){
 	   System.out.println("Test conversion from Fahrenheit to Kelvin....") ;
 	   
-	   Temperature averageHighInOctober = new Temperature (32.0, Temperature.Units.FAHRENHEIT);
-	   Temperature converted = new Temperature(averageHighInOctober);
+	   Temperature temp = new Temperature (32.0, Temperature.Units.FAHRENHEIT);
+	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.KELVIN);
 	   
 	   assertTrue(converted.getValue() == 273.15) ;
@@ -50,8 +50,8 @@ public class TemperatureTest {
    public void test_kelvinToFahrenheit(){
 	   System.out.println("Test conversion from Kelvin to Fahrenheit....") ;
 	   
-	   Temperature averageHighInOctober = new Temperature (273.15, Temperature.Units.KELVIN);
-	   Temperature converted = new Temperature(averageHighInOctober);
+	   Temperature temp = new Temperature (273.15, Temperature.Units.KELVIN);
+	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.FAHRENHEIT);
 	   
 	   assertTrue(converted.getValue() == 32.0) ;
@@ -61,8 +61,8 @@ public class TemperatureTest {
    public void test_celsiusToFahrenheit(){
 	   System.out.println("Test conversion from Celsius to Fahrenheit....") ;
 	   
-	   Temperature averageHighInOctober = new Temperature (0.0, Temperature.Units.CELSIUS);
-	   Temperature converted = new Temperature(averageHighInOctober);
+	   Temperature temp = new Temperature (0.0, Temperature.Units.CELSIUS);
+	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.FAHRENHEIT);
 	   
 	   assertTrue(converted.getValue() == 32.0) ;
@@ -72,8 +72,8 @@ public class TemperatureTest {
    public void test_fahrenheitToCelsius(){
 	   System.out.println("Test conversion from Fahrenheit to Celsius") ;
 	   
-	   Temperature averageHighInOctober = new Temperature (32.0, Temperature.Units.FAHRENHEIT);
-	   Temperature converted = new Temperature(averageHighInOctober);
+	   Temperature temp = new Temperature (32.0, Temperature.Units.FAHRENHEIT);
+	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.CELSIUS);
 	   
 	   assertTrue(converted.getValue() == 0.0) ;
