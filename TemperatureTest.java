35cbe2f685f4ac4ce0c68533bd3c4b37bb299c6f
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-10-14 19:20:12 -0400

    Commented the code to descibe the test cases and make the code more easily understandable

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -13,70 +13,70 @@ public class TemperatureTest {
   // {@Code Temperature} class.
 
    @Test
-   public void test_celsiusToKelvin(){											//Tests conversion from Celcius to Kelvin
+   public void test_celsiusToKelvin(){											//Tests conversion from Celsius to Kelvin
 	   System.out.println("Test conversion from Celsius to Kelvin....") ;
 	   
-	   Temperature temp = new Temperature (0.0, Temperature.Units.CELSIUS);		//Easiest case of converting 0 Celcius to Kelvin
+	   Temperature temp = new Temperature (0.0, Temperature.Units.CELSIUS);		//Easiest case of converting 0 Celcius
 	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.KELVIN);
 	   
-	   assertTrue(converted.getValue() == 273.15) ;
+	   assertTrue(converted.getValue() == 273.15) ;								//Result should be 273.15 Kelvin 
    }
 
    @Test
-   public void test_kelvinToCelsius(){
+   public void test_kelvinToCelsius(){											//Tests conversion from Kelvin to Celsius 
 	   System.out.println("Test conversion from Kelvin to Celsius....") ;
 	   
-	   Temperature temp = new Temperature (273.15, Temperature.Units.KELVIN);
+	   Temperature temp = new Temperature (273.15, Temperature.Units.KELVIN);	//Easiest case of converting 273.15 Kelvin
 	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.CELSIUS);
 	   
-	   assertTrue(converted.getValue() == 0.0) ;
+	   assertTrue(converted.getValue() == 0.0) ;								//Result should be 0 Celsius 
       
    }
    
    @Test
-   public void test_fahrenheitToKelvin(){
+   public void test_fahrenheitToKelvin(){										//Tests conversion from Fahrenheit to Kelvin
 	   System.out.println("Test conversion from Fahrenheit to Kelvin....") ;
 	   
-	   Temperature temp = new Temperature (32.0, Temperature.Units.FAHRENHEIT);
+	   Temperature temp = new Temperature (32.0, Temperature.Units.FAHRENHEIT);	//Easiest case of converting 32 Fahrenheit
 	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.KELVIN);
 	   
-	   assertTrue(converted.getValue() == 273.15) ;
+	   assertTrue(converted.getValue() == 273.15) ;								//Result should be 273.15 Kelvin
    }
    
    @Test
-   public void test_kelvinToFahrenheit(){
+   public void test_kelvinToFahrenheit(){										//Tests conversion from Kelvin to Fahrenheit
 	   System.out.println("Test conversion from Kelvin to Fahrenheit....") ;
 	   
-	   Temperature temp = new Temperature (273.15, Temperature.Units.KELVIN);
+	   Temperature temp = new Temperature (273.15, Temperature.Units.KELVIN);	//Easiest case of converting 273.15 Kelvin
 	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.FAHRENHEIT);
 	   
-	   assertTrue(converted.getValue() == 32.0) ;
+	   assertTrue(converted.getValue() == 32.0) ;								//Result should be 32 Fahrenheit
    }
    
    @Test
-   public void test_celsiusToFahrenheit(){
+   public void test_celsiusToFahrenheit(){										//Tests conversion from Celsius to Fahreheit 
 	   System.out.println("Test conversion from Celsius to Fahrenheit....") ;
 	   
-	   Temperature temp = new Temperature (0.0, Temperature.Units.CELSIUS);
+	   Temperature temp = new Temperature (0.0, Temperature.Units.CELSIUS);		//Easiest case of converting 0 Celsius 
 	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.FAHRENHEIT);
 	   
-	   assertTrue(converted.getValue() == 32.0) ;
+	   assertTrue(converted.getValue() == 32.0) ;								//Result should be 32 Fahrenheit 
    }
    
    @Test
-   public void test_fahrenheitToCelsius(){
+   public void test_fahrenheitToCelsius(){										//Tests conversion from Fahrenheit to Celsius 
 	   System.out.println("Test conversion from Fahrenheit to Celsius") ;
 	   
-	   Temperature temp = new Temperature (32.0, Temperature.Units.FAHRENHEIT);
+	   Temperature temp = new Temperature (32.0, Temperature.Units.FAHRENHEIT);	//Easiest case of converting 32 Fahrenheit
 	   Temperature converted = new Temperature(temp);
 	   converted.changeUnits(Temperature.Units.CELSIUS);
 	   
-	   assertTrue(converted.getValue() == 0.0) ;
+	   assertTrue(converted.getValue() == 0.0) ;								//Result should be 0 Celsius 
    }
    
 }
